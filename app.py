1162a073320cc808cbaa9147a0d29ed002cbb89f
王沛晟 <115681033@qq.com>
2018-11-08 19:12:09 +0800

    switch off the debug mode
    switch off the debug mode

--- app.py
+++ app.py
@@ -76,5 +76,5 @@ def dashboard():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True, host=('0.0.0.0'))
-    app.run(debug=True)
+    app.run(debug=True, host=('0.0.0.0'))
+    #app.run(debug=True)
